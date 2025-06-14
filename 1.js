7ee541d7f0f7da374819c568cb048075ac5155aa
lampaserg <serdjik73@mail.ru>
2025-06-14 20:51:38 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,5 @@
 !function() {
     "use strict";
-    if (window.location.hostname === 'cf.lampa.mx') {
-    	return true;
-	}
     if(Lampa.Storage.get("cub_fix")=='2') $('.noty').hide(); // для тех кто не обновил lampac
     window.lampa_settings.torrents_use = true;
 	window.lampa_settings.demo = false;
